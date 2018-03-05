357c38e63d1e81ce8d1887d75fab0f92ed0030e5
Motaz Saad <motaz.saad@gmail.com>
2018-03-05 04:25:51 +0000

    fix bug

--- wav_list.sh
+++ wav_list.sh
@@ -7,14 +7,13 @@ fi
 
 function make_wav_list {
     in_dir=${1}
-    mkdir -p ${out_dir}
     for wav_file in ${in_dir}*.wav
     do
-        duration=$(sox --i -D "$file")
+        duration=$(sox --i -D "$wav_file")
         printf "%s\t%s" ${wav_file} ${duration}
     done
 }
 
-make_wav_list ${1} ${2}
+make_wav_list ${1}
 
 echo "done!"
