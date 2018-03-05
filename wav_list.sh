2d71e31d5dae3d7761d3972ae5c86d511800821d
Motaz Saad <motaz.saad@gmail.com>
2018-03-05 04:36:36 +0000

    fix bug

--- wav_list.sh
+++ wav_list.sh
@@ -11,7 +11,7 @@ function make_wav_list {
     do
         duration=$(sox --i -D "$wav_file")
         file_name=$(basename ${wav_file})
-        printf "%.1f\t%s\n" ${file_name} ${duration}
+        printf "%.1f\t%s\n" ${duration} ${file_name}
     done
 }
 
